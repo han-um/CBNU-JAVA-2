67250647d53447d2d992b7f2c3b09b647532906e
hanum <hanum@192.168.0.152>
2017-11-06 18:55:22 +0900

    sf

---
+++ 20171013Exercise/src/Exercise6_1/Exercise6_1.java
@@ -0,0 +1,24 @@
+package Exercise6_1;
+
+class SutdaCard{
+	int num;
+	boolean isKwang;
+	SutdaCard(){
+		this(1,true);
+	}
+	SutdaCard(int num, boolean isKwang){
+		this.num = num;
+		this.isKwang = isKwang;
+	}
+	public String  info(){
+		
+	}
+}
+
+public class Exercise6_1 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
